d57ab64cee9566889928571a0ca61fe339cc3528
Arshaan <arshanss504@gmail.com>
2025-02-27 01:58:32 +0530

    Delete K8 with PD directory

--- K8 with PD/app/__init__.py
+++
@@ -1,3 +0,0 @@
-#If you omit __init__.py, Python may not recognize the directory as a package, and you won't be able to import modules from it.
-# Example: from app import routes
-# you cannot import routes.py if __init__.py is missing in the same directory.
--- K8 with PD/app/config.py
+++
@@ -1,7 +0,0 @@
-#config.py is a Python file commonly used in projects to store configuration settings, such as API keys, database credentials, environment variables, and application-specific constants.
-# The config.py file is usually located in the app directory of a project and is imported into other modules to access configuration settings.
-# The config.py file can be used to define global variables, constants, and settings that are used throughout the project.
-# example: db_url = "mysql://user:password@localhost/db_name", api_key = "1234567890", log_level = "INFO"
-
-
-# Instead of hardcoding settings in multiple files, config.py provides a single location for all configurations.
